90fa39c4e6df10e1b8f1213c8bad31ce168d126c
Roman <vladimirovrm@gmail.com>
2022-12-26 21:12:32 +0300

    create app

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+import './search-bos.styles.css'
+
+
+class SearchBox extends Component {
+	render() {
+		const { onChangeHandler, className, placeholder } = this.props;
+		return (
+			<input
+				className={`search-box ${className}`}
+				type='search'
+				placeholder={placeholder}
+				onChange={onChangeHandler}
+			/>
+		);
+	}
+}
+
+export default SearchBox;
