540d962b645c1b4d30d37ec80b19d3059d3591e5
Roman <vladimirovrm@gmail.com>
2022-12-27 15:53:01 +0300

    add  useEffect

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ import './App.css';
 const App = () => {
 	const [searchValue, setSearchValue] = React.useState('');
 	const [monsters, setMonsters] = React.useState([]);
+	const [filteredMonsters, setFilteredMonsters] = React.useState(monsters);
 
+	console.log('rendered');
 
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
 			.then((users) => setMonsters(users));
-
 	}, []);
+
+	useEffect(() => {
+		const newFilteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLowerCase().includes(searchValue);
+		});
+
+		setFilteredMonsters(newFilteredMonsters);
+	}, [monsters, searchValue]);
 
 	const onSearchChange = (event) => {
 		const searchFieldString = event.target.value.toLowerCase();
 		setSearchValue(searchFieldString);
 	};
-
-	const filteredMonsters = monsters.filter((monster) => {
-		return monster.name.toLowerCase().includes(searchValue);
-	});
 
 	return (
 		<div className='App'>
@@ -34,6 +39,7 @@ const App = () => {
 				className='monsters-search-box'
 				onChangeHandler={onSearchChange}
 			/>
+
 			<CardList monsters={filteredMonsters} />
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
 );
-
 reportWebVitals();
